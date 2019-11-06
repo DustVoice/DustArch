cd8724f083c35d7d6e5599f8a95d5fdf90d1b86e
DustVoice <info@dustvoice.de>
2019-11-06 22:05:13 +0100

    Finalize customize_airootfs.sh

--- livecd/airootfs/root/customize_airootfs.sh
+++ livecd/airootfs/root/customize_airootfs.sh
@@ -37,12 +37,11 @@ sudo -u dustvoice gem install asciidoctor-epub3 --pre
 sudo -u dustvoice gem install pygments.rb --pre
 
 sudo -u dustvoice nvim --headless +PlugInstall +qa
-sudo -u dustvoice python3 ~/.config/nvim/plugged/YouCompleteMe/install.py --clang-completer
+sudo -u dustvoice python3 /home/dustvoice/.config/nvim/plugged/YouCompleteMe/install.py --clang-completer
 
 sudo -u dustvoice gpg-connect-agent updatestartuptty /bye
 
-sudo -u dustvoice loadkeys custom-us
-sudo -u dustvoice localectl set-keymap --no-convert custom-us
+sudo localectl set-keymap --no-convert custom-us
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
