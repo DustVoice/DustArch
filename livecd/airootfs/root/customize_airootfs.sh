ee0863e37673356a2eab28d5eaa129a00b1189ea
DustVoice <info@dustvoice.de>
2019-11-06 02:49:43 +0100

    Fully functional DustArch; TODO: Add documentation for DustArch archiso creation process

--- livecd/airootfs/root/customize_airootfs.sh
+++ livecd/airootfs/root/customize_airootfs.sh
@@ -27,7 +27,14 @@ sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
+pip3 install neovim
 pip3 install pygments
+
+! id dustvoice && useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/fish dustvoice
+echo 'dustvoice ALL=(ALL) ALL' >> /etc/sudoers
+
+su -s /bin/bash dustvoice
+
 gem install asciidoctor --pre
 gem install asciidoctor-pdf --pre
 gem install asciidoctor-epub3 --pre
@@ -35,6 +42,12 @@ gem install pygments.rb --pre
 
 nvim --headless +PlugInstall +qa
 python3 ~/.config/nvim/plugged/YouCompleteMe/install.py --clang-completer
+
+gpg-connect-agent updatestartuptty /bye
+
+exit
+
+usermod -s /usr/bin/fish dustvoice
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
@@ -45,4 +58,10 @@ systemctl start dhcpcd.service
 systemctl enable pcscd
 systemctl start pcscd
 
-gpg-connect-agent updatestartuptty /bye
+echo ""
+echo "Please set a password for the [root] user:"
+until passwd; do echo "Try again, or Ctrl-c within 5 seconds!"; sleep 5; done
+
+echo ""
+echo "Please set a password for the [dustvoice] user:"
+until passwd dustvoice; do echo "Try again, or Ctrl-c within 5 seconds!"; sleep 5; done
